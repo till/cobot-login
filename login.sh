6876b18a6d2ce21c1ad9c5bf84f346ecdfbe4432
Till Klampaeckel <till@php.net>
2010-08-30 14:34:48 +0200

    * auto-discover captive portal url

--- login.sh
+++ login.sh
@@ -10,18 +10,54 @@ this_dir=$(dirname $0)
 user_config="${this_dir}/etc/user.cfg"
 space_config="${this_dir}/etc/space.cfg"
 
-if [ ! -f $user_config ]; then
-    echo "Please create a user config: ${user_config}"
-    exit 1
-fi
-
 if [ ! -f $space_config ]; then
     echo "Please create a space config: ${space_config}"
     exit 1
 fi
 
-source $user_config
+
+if [ `uname -s` = "Linux" -o `uname -s` = "FreeBSD" -o `uname -s` = "NetBSD" -o `uname -s` = "OpenBSD" ]; then
+
+    if [ ! -f $user_config ]; then
+        echo "Please create a user config: ${user_config}"
+        exit 1
+    fi
+
+    source $user_config
+
+elif [ `uname -s` = "Darwin" ]; then
+
+    echo "Use the keychain! Not yet implemented."
+    exit 1;
+
+else
+
+    # do something else
+    echo "Unknown OS: $(uname -s)" 
+    exit 1
+
+fi
+
 source $space_config
+
+discoverCaptivePortal () {
+    local url=$(curl -w=%{redirect_url} -o /dev/null -s -I http://www.google.de/)
+
+    if [ "$url" = "=" ]; then
+        echo ""
+    fi
+
+    echo "${url:1}"
+}
+
+cobot_captiveportal_url=$(discoverCaptivePortal)
+if [ "x${cobot_captiveportal_url}" = "x" ]; then
+    echo "Logged in already."
+    exit 0
+fi
+
+echo $cobot_captiveportal_url
+exit;
 
 data="username=${cobot_username}&redirect_url=&auth_user=${cobot_username}${cobot_username_postfix}&accept=Log+In&account_type=${cobot_account_type}"
 cmd="curl -X POST -w=%{response_code} -s -o /dev/null -d $data --data-urlencode auth_pass=${cobot_pass} $cobot_captiveportal_url"
